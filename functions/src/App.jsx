8939d109c98dce99bf3699796832ea0135e4702e
Saurabhparaskar <saurabhparaskar28@gmail.com>
2026-06-03 21:28:30 +0530

    function calling

---
+++ functions/src/App.jsx
@@ -0,0 +1,35 @@
+const App =()=>{
+
+    function btnClicked(){
+        console.log('button is clicked');
+    }
+    function inputChanging(){
+        console.log('Typingg ..');
+    }
+
+     
+    return(
+        <div>
+            <h1> APP Function calling ...</h1>
+            <button  onClick={btnClicked} >Change user</button>  
+             
+             {/* one time called beacused () */}
+             <button   onClick={btnClicked()}>OutsideFunction()</button>     
+            
+            {/* inline function... */}
+            <button onChange ={ ()=>{
+
+                console.log('ButtoninsideFunction');
+            }}>ButtoninsideFunction</button>
+       
+
+       <input onChange={function (elem){
+        console.log(elem.target.value);
+
+       }} type="text " placeholder="Enter name "></input>
+       
+        </div>
+    )
+}
+
+export default App
